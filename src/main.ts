886b628f57f245aefb0164ff1edd51bf342cee48
Serhii Kostenko <fortedeimarmi21@gmail.com>
2020-10-23 04:01:17 +0200

    Revert "configure package and remove old files"
    This reverts commit 406dcb1fd8bf061f605a84df8974d7acf8658abd.

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+
+// import { getPackageTypes } from 'npm-package-types-fetch/build/src';
+
+//example
+// (async () => {
+//   const types = await getPackageTypes('react', '16.14.0');
+//   console.log('package types', types);
+// })()
+
+// examples
+// (async () => {
+//   const types = await getPackageTypes('iframe_communication', '1.1.3');
+//   console.log('package types', types);
+// })()
